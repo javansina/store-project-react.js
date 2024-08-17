504065f96dc7aab4876469fec6d1a1b981f21584
sina javan <javansina.dev@gmail.com>
2024-08-18 00:38:25 +0330

    Pages and routes were created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import ProductsPage from './pages/ProductsPage';
+import Detailspage from './pages/Detailspage';
+import CheckoutPage from './pages/CheckoutPage';
+import PageNotFound from './pages/404';
+
 function App() {
    return (
       <>
-         <h1>Hello Galaxy</h1>
+         <Routes>
+            <Route index element={<Navigate to="/products" replace />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products/:id" element={<Detailspage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="*" element={<PageNotFound />} />
+         </Routes>
       </>
    );
 }
