6310592b11bfc6aa681205135c0932496151f1b0
sina javan <javansina.dev@gmail.com>
2024-08-18 01:49:16 +0330

    Creating an instance of axios and intercept responses

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,16 +1,4 @@
-
-import { useEffect, useState } from 'react';
-import { api } from '../services/config';
-
-
 function ProductsPage() {
-  const [moz, setMoz] = useState([]);
-
-  useEffect(() => {
-     api.get('/products').then((res) => setMoz(res));
-  }, []);
-
-  console.log(moz);
    return <div>products</div>;
 }
 
